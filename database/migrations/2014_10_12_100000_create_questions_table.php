30c51d6117d8f194aa6786a0cd1f334dc9f1701e
huss0202 <huss0202@umn.edu>
2017-07-08 12:02:01 -0500

    migration refresh

--- database/migrations/2014_10_12_100000_create_questions_table.php
+++ database/migrations/2014_10_12_100000_create_questions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateQuestionCheckTable extends Migration
+class CreateQuestionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,10 @@ class CreateQuestionCheckTable extends Migration
      */
     public function up()
     {
-        Schema::create('question_check', function (Blueprint $table) {
-            $table->increments('check_id');
-            $table->integer('ques_id');
-            $table->string('question_to_check');
-            $table->tinyInteger('has_been_answered');
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('question_id');
+            $table->string('question');
+            $table->string('asker_email');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -30,6 +29,6 @@ class CreateQuestionCheckTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('question_check');
+        Schema::dropIfExists('questions');
     }
 }
