36259dba1e81dc6f9e923aa2a1a721bc6d7c9fe9
huss0202 <huss0202@umn.edu>
2017-07-13 08:54:39 -0500

    fixing imagemanager use

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,8 +13,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redirect;
-//use Intervention\Image\ImageManagerStatic as Image;
-//use Intervention\Image\ImageManager;
+
 class UserController extends Controller {
 
 
