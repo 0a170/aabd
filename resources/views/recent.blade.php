8ecaf913196efcd09eeecd45d22bd0768cf0235b
huss0202 <huss0202@umn.edu>
2017-07-14 09:44:53 -0500

    changing button styles

--- resources/views/recent.blade.php
+++ resources/views/recent.blade.php
@@ -10,7 +10,7 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link rel="stylesheet" type="text/css" href="{{asset('css/rate_style.css')}}"> 
+<link rel="stylesheet" type="text/css" href="{{asset('css/rate_style.css')}}">
 
 
 
@@ -29,7 +29,7 @@
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
+        <span class="icon-bar"></span>
       </button>
       <a class="navbar-brand" href="{{ url('/ask') }}">Ask a Brotha</a>
     </div>
@@ -42,16 +42,16 @@
         <li><a href="{{ url('/users') }}" class="link4"><span class="glyphicon glyphicon-user"></span> Users</a></li>
         <li>
             <a href="#" onclick="event.preventDefault();
-                                 document.getElementById('logOutForm').submit();" class="link3"> 
-            <span class="glyphicon glyphicon-log-out"></span> 
+                                 document.getElementById('logOutForm').submit();" class="link3">
+            <span class="glyphicon glyphicon-log-out"></span>
             Logout</a>
-        
+
                <form id="logOutForm" method="POST" action="{{ url('/logout') }}" style="display: none;">
-                  
-                  {{ csrf_field() }}             
-               
+
+                  {{ csrf_field() }}
+
                </form>
-                     
+
         </li>
       @else
         <li><a href="{{ url('/ask') }}" class="link1"><span class="glyphicon glyphicon-question-sign"></span> Ask</a></li>
@@ -68,84 +68,84 @@
 
 <div class="div1">
 
-            
+
                 <h1> Recent Answers </h1>
-                              
-                              
+
+
                   @foreach($answers as $answer)
-                  
+
                   <br>
-                  
+
                   <div class="rateDiv">
-                
+
                     <form id="{{ $answer->answer_id }}" class="rateForm" method="POST">
-                        
+
                         <br>
                         <input type="hidden" value="{{ $answer->user_id }}" name="UIDName">
                         <input type="hidden" value="{{ $answer->answer_score }}" name="answerScoreName">
-                        <input type="hidden" value="{{ $answer->user_answer }}" name="answeredQuestionName"> 
-                        <input type="hidden" value="{{ $answer->up_votes }}" name="upVoteName"> 
+                        <input type="hidden" value="{{ $answer->user_answer }}" name="answeredQuestionName">
+                        <input type="hidden" value="{{ $answer->up_votes }}" name="upVoteName">
                         <input type="hidden" value="{{ $answer->down_votes }}" name="downVoteName">
                         <input type="hidden" value="{{ $answer->answer_id }}">
-                        
+
                         <input type="hidden" value="{{ csrf_token() }}">
-                        
-                     
+
+
                         <p> Question: {{ $answer->answered_question }} </p>
                         <br>
                         <p> User Answer: {{ $answer->user_answer }} </p>
                         <br>
                         <p> User ID: {{ $answer->user_id }} </p>
-                        
+
                         <br>
-                        
+
                         <button type="button" id="voteButton" class="btn btn-default btn-lg" value="upButtonVal">
                            <span class="glyphicon glyphicon-thumbs-up">
                               {{ $answer->up_votes }}
-                           </span> 
+                           </span>
                         </button>
-                        
+
                         &nbsp &nbsp &nbsp &nbsp
-                        
+
                         <button type="button" id="voteButton" class="btn btn-default btn-lg" value="downButtonVal">
-                           <span class="glyphicon glyphicon-thumbs-down"> 
-                              {{ $answer->down_votes }} 
-                           </span> 
+                           <span class="glyphicon glyphicon-thumbs-down">
+                              {{ $answer->down_votes }}
+                           </span>
                         </button>
-                        
+
                         <br>
                         <br>
-                        
+
                         <div id="rate_failure{{ $answer->answer_id }}" class="ajax_failure"></div>
-                        
+
                     </form>
-                    
+
                   </div>
-                  
+
                   <br>
                   <br>
-                 @endforeach   
-            
-                
+                 @endforeach
+
+
                 <br>
-                   
- 
+
+
 </div>
 
-   
+
 
 </body>
 
 <script>
 
    $(document).ready(function() {
-       
+
       $("#logOutForm").submit(function(e) {
-         
-         e.preventDefault(); 
-                 
+
+         e.preventDefault();
+
       });
-      
+
    });
 
 </script>
--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -30,7 +30,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Ask a Brotha</a>
+      <a class="navbar-brand" href="{{ url('/ask') }}">Ask a Brotha</a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
 
