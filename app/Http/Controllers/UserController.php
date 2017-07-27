5c6f47898faedfb2aa72ba166b71314729386c33
huss0202 <huss0202@umn.edu>
2017-07-27 08:32:07 -0500

    too lazy to test locally

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -92,7 +92,7 @@ class UserController extends Controller {
 
          $file_avatar = $file_avatar->stream();
 
-         Storage::disk('s3')->put('profile_images/' . $username, $file_avatar->__toString());
+         Storage::disk('s3')->put('profile_images/' . $username_sans_ext . '.' . $ext, $file_avatar->__toString());
          //$file->storeAs('/public/storage/images/', $username);
 
          //$manager = new ImageManager(array('driver' => 'imagick'));
