8b73dc52b7cb0aebc7951cba5440bacc1a73fbe8
huss0202 <huss0202@umn.edu>
2017-07-29 17:11:08 -0500

    still styling and profiling

--- resources/views/user_profile.blade.php
+++ resources/views/user_profile.blade.php
@@ -18,7 +18,7 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
+<script src="{{ asset('js/rate_ajax.js') }}"></script>
 
 </head>
 
@@ -105,67 +105,62 @@
 
    <div class="div2">
 
+      <h1> {{ $user->user_name }}'s answers </h1>
 
-                   <h1> Recent Answers </h1>
+      <br>
 
+      @foreach($user_answers as $user_answer)
 
-                     @foreach($answers as $answer)
+         <div class="rateDiv">
 
-                     <br>
+            <form id="{{ $answer->answer_id }}" class="rateForm" method="POST">
 
-                     <div class="rateDiv">
+               <br>
+               <input type="hidden" value="{{ $answer->user_id }}" name="UIDName">
+               <input type="hidden" value="{{ $answer->answer_score }}" name="answerScoreName">
+               <input type="hidden" value="{{ $answer->user_answer }}" name="answeredQuestionName">
+               <input type="hidden" value="{{ $answer->up_votes }}" name="upVoteName">
+               <input type="hidden" value="{{ $answer->down_votes }}" name="downVoteName">
+               <input type="hidden" value="{{ $answer->answer_id }}">
 
-                       <form id="{{ $answer->answer_id }}" class="rateForm" method="POST">
+               <input type="hidden" value="{{ csrf_token() }}">
 
-                           <br>
-                           <input type="hidden" value="{{ $answer->user_id }}" name="UIDName">
-                           <input type="hidden" value="{{ $answer->answer_score }}" name="answerScoreName">
-                           <input type="hidden" value="{{ $answer->user_answer }}" name="answeredQuestionName">
-                           <input type="hidden" value="{{ $answer->up_votes }}" name="upVoteName">
-                           <input type="hidden" value="{{ $answer->down_votes }}" name="downVoteName">
-                           <input type="hidden" value="{{ $answer->answer_id }}">
+               <p> Question: {{ $user_answer->answered_question }} </p>
+               <br>
+               <p> Answer: {{ $user_answer->user_answer }} </p>
 
-                           <input type="hidden" value="{{ csrf_token() }}">
+               <br>
 
+               <button type="button" id="voteButton" class="btn btn-default btn-lg" value="upButtonVal">
+                  <span class="glyphicon glyphicon-thumbs-up">
+                     {{ $user_answer->up_votes }}
+                  </span>
+               </button>
 
-                           <p> Question: {{ $answer->answered_question }} </p>
-                           <br>
-                           <p> User Answer: {{ $answer->user_answer }} </p>
-                           <br>
-                           <p> User ID: {{ $answer->user_id }} </p>
+               &nbsp &nbsp &nbsp &nbsp
 
-                           <br>
+               <button type="button" id="voteButton" class="btn btn-default btn-lg" value="downButtonVal">
+                  <span class="glyphicon glyphicon-thumbs-down">
+                     {{ $user_answer->down_votes }}
+                  </span>
+               </button>
 
-                           <button type="button" id="voteButton" class="btn btn-default btn-lg" value="upButtonVal">
-                              <span class="glyphicon glyphicon-thumbs-up">
-                                 {{ $answer->up_votes }}
-                              </span>
-                           </button>
+               <br>
+               <br>
 
-                           &nbsp &nbsp &nbsp &nbsp
-
-                           <button type="button" id="voteButton" class="btn btn-default btn-lg" value="downButtonVal">
-                              <span class="glyphicon glyphicon-thumbs-down">
-                                 {{ $answer->down_votes }}
-                              </span>
-                           </button>
-
-                           <br>
-                           <br>
-
-                           <div id="rate_failure{{ $answer->answer_id }}" class="ajax_failure"></div>
-
-                       </form>
-
-                     </div>
-
-                     <br>
-                     <br>
-                    @endforeach
+               <div id="rate_failure{{ $user_answer->answer_id }}" class="ajax_failure"></div>
 
 
-                   <br>
+            </form>
 
+         </div>
+
+         <br>
+         <br>
+
+      @endforeach
+
+      <br>
 
    </div>
 
