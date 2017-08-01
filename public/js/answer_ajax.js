82a5dac16c9ae4fe53614fb8b3db6e4f90eb951b
huss0202 <huss0202@umn.edu>
2017-08-01 12:10:13 -0500

    trying to fix answer emailing

--- public/js/answer_ajax.js
+++ public/js/answer_ajax.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	
+
 	//$(".aForm").hide();
 
 	$.ajaxSetup({
@@ -7,24 +7,24 @@ $(document).ready(function() {
         'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
     	}
 	});
-	
+
 	$('.aForm').on('submit', function(e) {
-			
-		
+
+
 		//alert("clicked");
 		//e1.stopPropagation();
 		e.preventDefault();
 		var theForm = $(this);
 		//alert(theForm);
-		
-		
-		var fID = theForm.attr('id');		
-		var dID = $('#' + fID).parent().attr('id'); 
+
+
+		var fID = theForm.attr('id');
+		var dID = $('#' + fID).parent().attr('id');
 		//alert(fID);
 
 	   //alert("The form ID: " + fID);
 
-		//var ansID = $('#' + fID + ' textarea').attr('id'); 
+		//var ansID = $('#' + fID + ' textarea').attr('id');
 		var ansID = $('#' + fID + ' :nth-child(1)').attr('id');
 		var ansVal = $('#' + ansID).val();
 		var questID = $('#' + fID + ' :hidden:eq(0)').attr('id');
@@ -33,60 +33,60 @@ $(document).ready(function() {
 		var emailVal = $('#' + emailID).val();
 		var userID = $('#' + fID + ' :hidden:eq(2)').attr('id');
 
-		
+
 		//var failureID = $('#' + successID).next().attr('id');
 		var failureID = $('#' + fID + ' :nth-child(8)').attr('id');
 		var successID = $('#' + fID + ' :nth-child(9)').attr('id');
 		var servFailureID = $('#' + fID + ' :nth-child(10)').attr('id');
 		var token = $('#' + fID + ' :nth-child(4)').attr('id');
-		
+
 
 		if(ansVal == '') {
-				
+
 			$('#' + failureID).show().html("Answer Cannot be empty");
 			setTimeout(function() {
 				$('#' + failureID).fadeOut("slow");
-				}, 3000);  
-				
+				}, 3000);
+
 			//$('#' + ansID).css("background-color", "red");
-				
+
 		} else {
 
 			$.ajax({
-			
+
 				type: "POST",
 				url: "answer",
 				data: {'answerInput': ansVal, 'ques': questVal, 'ema': emailVal, '_token': token},
 				//datatype: 'text',
 				cache: false,
-			
+
 				success: function(data) {
-				
+
 					//alert(data);
 					$('#' + successID).show().html("Successfully sent");
 					//setTimeout(function() {
 					$('#' + dID).fadeOut(3000, function() {
-						$('#' + dID).remove();	
+						$('#' + dID).remove();
 					});
-				
+
 				},
 				error: function(data) {
-					
+
 					//alert(ansVal);
-					$('#' + servFailureID).show().html("Server Issue, try again later");
+					$('#' + servFailureID).show().html("Server Issue: " + data + "try again later");
 					//setTimeout(function() {
 					$('#' + servFailureID).fadeOut(3000, function() {
-						$('#' + servFailureID).remove();	
+						$('#' + servFailureID).remove();
 					});
-					
+
 				}
-			
-			});  
-			
+
+			});
+
 			//return false;
 		}
-		
+
 	});
-	
-	
+
+
 });
