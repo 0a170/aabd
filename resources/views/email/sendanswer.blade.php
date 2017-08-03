2e28ee0cbc7a8b70a60b9666f1c1486e9e5630f8
huss0202 <huss0202@umn.edu>
2017-08-03 16:46:43 -0500

    styling email

--- resources/views/email/sendanswer.blade.php
+++ resources/views/email/sendanswer.blade.php
@@ -2,7 +2,56 @@
 <html>
 <body>
 
-<p> {{ $body }} </p>
+<head>
+
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+   <link rel="stylesheet" type="text/css" href="{{ asset('email_style.css') }}">
+
+</head>
+
+<body style="text-align: center;">
+
+<div class="container">
+
+   <table style="width: 100%; max-width: 600px;" border="0" cellpadding="0" cellspacing="0">
+
+    <tr>
+     <td style="background: #80bfff">
+      <img src="logo.png" style="display: block; margin: auto;">
+     </td>
+    </tr>
+
+    <tr>
+     <td>
+      <br>
+      <br>
+      <br>
+      {{ $body }}
+      <br>
+      <br>
+      <a href="{{ url('/ask') }}" class="btn btn-primary btn-lg" role="button">Ask Again</a>
+      <br>
+      <br>
+     </td>
+    </tr>
+
+    <tr>
+     <td class="footer">
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+     </td>
+    </tr>
+
+   </table>
+
+</div>
 
 </body>
 </html>
