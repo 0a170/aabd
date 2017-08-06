0fb705244d96bbecf3f854f48d3d66aa84302ba1
huss0202 <huss0202@umn.edu>
2017-08-06 08:43:50 -0500

    fixing blade error in home

--- resources/views/user_profile.blade.php
+++ resources/views/user_profile.blade.php
@@ -42,7 +42,7 @@
 
       @if (Auth::check())
 
-      ``<li><a href="{{ url('/home') }}" class="link5"><span class="glyphicon glyphicon-pencil"></span> Answer </a></li>
+        <li><a href="{{ url('/home') }}" class="link5"><span class="glyphicon glyphicon-pencil"></span> Answer </a></li>
         <li><a href="{{ url('/ask') }}" class="link1"><span class="glyphicon glyphicon-question-sign"></span> Ask </a></li>
         <li><a href="{{ url('/recent') }}" class="link2"><span class="glyphicon glyphicon-thumbs-up"></span> Recent </a></li>
         <li><a href="{{ url('/users') }}" class="link4"><span class="glyphicon glyphicon-user"></span> Users </a></li>
