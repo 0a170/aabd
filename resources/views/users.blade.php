93693107ac4183845436e3ba0e74479f5f7041a7
huss0202 <huss0202@umn.edu>
2017-08-14 08:57:13 -0500

    yep still trying to fix user search

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -93,12 +93,12 @@
       <br>
       <br>
 
-      <div class="testDiv">
+
 
          <input type="text" name="search" class="testInput">
          <div class="results"></div>
 
-      </div>
+   
 
       <br>
       <br>
