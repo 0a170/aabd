cb5f4044402ad4ac2e0207ff083e910f3daeb51e
huss0202 <huss0202@umn.edu>
2017-09-15 00:29:06 -0500

    fixing issues on recent answers page

--- app/Http/Controllers/AnsweredQuestionController.php
+++ app/Http/Controllers/AnsweredQuestionController.php
@@ -9,6 +9,7 @@ use App\User;
 use App\Mail\AnswerMail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Mail\Mailer;
 use Illuminate\Support\Facades\Redirect;
 
