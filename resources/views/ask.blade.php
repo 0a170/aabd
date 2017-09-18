1c388452edc99d424d2305fe7de89286ce6f00fc
huss0202 <huss0202@umn.edu>
2017-09-18 16:32:39 -0500

    general functionality fixes

--- resources/views/ask.blade.php
+++ resources/views/ask.blade.php
@@ -121,20 +121,24 @@
 
 		     <div id="success_message" class="ajax_response" style="float:left"></div>
            <div id="error_message" class="ajax_response" style"float:left"></div>
-           <br>
-           @if ($errors->any())
-             <div class="alert alert-danger">
-                 <ul>
-                    @foreach ($errors->all() as $error)
-                       <li> {{ $error }} </li>
-                    @endforeach
-                </ul>
-             </div>
-          @endif
 
 	     </div>
 
     </div>
+
+    <br>
+
+    @if ($errors->any())
+      <div class="alert alert-danger">
+          <ul>
+             @foreach ($errors->all() as $error)
+                <li> {{ $error }} </li>
+             @endforeach
+         </ul>
+      </div>
+   @endif
+
+
     </body>
 
 </html>
