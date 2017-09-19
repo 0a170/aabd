08b808b044298c560760f0a558311ac76068c250
huss0202 <huss0202@umn.edu>
2017-09-19 12:28:14 -0500

    fixing 404 page

--- routes/web.php
+++ routes/web.php
@@ -91,6 +91,6 @@ Route::get('/error', function() {
 
 
 //REDIRECT ANY UNKNOWN REQUEST NOT FOUND ABOVE BACK TO HOMEPAGE
-Route::any('{query}',
+/*Route::any('{query}',
   function() { return redirect('/'); })
-  ->where('query', '.*');
+  ->where('query', '.*'); */
