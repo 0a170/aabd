34b2430d0dde5060994bce038fcd793c9461fef8
huss0202 <huss0202@umn.edu>
2017-09-19 12:57:33 -0500

    fixing 404 page

--- resources/views/notfound.blade.php
+++ resources/views/notfound.blade.php
@@ -11,7 +11,7 @@
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
-<link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}">
+
 <link rel="stylesheet" type="text/css" href="{{ asset('css/jquery.steps.css') }}">
 
 
