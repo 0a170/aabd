8443a50b72dcdff4494eb8d873aaf4ca06449850
mahussensr <spawnificent@gmail.com>
2017-11-04 23:45:56 -0500

    making all pages use the same stylesheet

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -10,7 +10,7 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link rel="stylesheet" type="text/css" href="{{asset('css/rate_style.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
 
 
 
--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -11,7 +11,7 @@
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet"/>
-<link rel="stylesheet" type="text/css" href="{{ asset('css/style2.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
 
 
 
