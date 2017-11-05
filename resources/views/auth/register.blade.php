726850d82d8197c5fb024c4ad609c27cda77efd5
mahussensr <spawnificent@gmail.com>
2017-11-05 00:15:53 -0500

    making all pages use the same stylesheet

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,9 +11,6 @@
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
-<link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}">
-
-
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
