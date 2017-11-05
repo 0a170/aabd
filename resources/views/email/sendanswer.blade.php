3ccb6ae3d2e40545141675bdd38147977d931f41
mahussensr <spawnificent@gmail.com>
2017-11-04 20:38:38 -0500

    fixing answer email styling

--- resources/views/email/sendanswer.blade.php
+++ resources/views/email/sendanswer.blade.php
@@ -13,22 +13,9 @@
 
       @import url(https://fonts.googleapis.com/css?family=Roboto:300);
 
-      table {
-
-      }
-
-      .header {
-         background: #80bfff;
-      }
-
-      .footer {
-         background: #888888;
-      }
-
       p {
    	   font-family: "Roboto", sans-serif;
-   	   color: #888888;
-   	   //font-size: 20px;
+   	   color: #4981ce;
    	   font-size:1.1em;
          font-weight: 300;
    	   text-shadow: none;
@@ -36,23 +23,46 @@
 
       h2 {
    	   font-family: "Roboto", sans-serif;
-   	   color: #888888;
+   	   color: #4981ce;
    	   text-shadow: none;
+       text-align: center;
       }
 
       h1 {
    	   font-family: "Roboto", sans-serif;
    	   text-align: center;
-         color: #888888;
+         color: #4981ce;
    	   font-size: 36px;
    	   font-weight: 300;
       }
 
       .ask-button {
          color: white;
-         border: 1 px #888888;
-         background: #611BBD;
+         border: 1 px #4981ce;
+         background: #4981ce;
          border-color: #130269;
+         text-align: center;
+         border: 10px solid #4981ce;
+         border-radius: 5px;
+         display: inline-block;
+      }
+
+      td.header {
+         border-bottom: 3px solid #4981ce;
+         background: #80bfff;
+      }
+
+      td.footer {
+         border-top: 3px solid #4981ce;
+         background: #80bfff;
+      }
+
+      a {
+        text-decoration: none;
+        color: white;
+        font-size: 20px;
+        font-family: "Roboto", sans-serif;
+        font-weight: normal;
       }
 
    </style>
@@ -63,41 +73,42 @@
 
 <div class="container">
 
-   <table style="width: 100%; max-width: 600px;" border="1" cellpadding="0" cellspacing="0">
+   <table style="width: 100%; max-width: 600px;" border="0" cellpadding="0" cellspacing="0">
 
-    <tr>
-     <td bgcolor="#80bfff" class="header">
-       <h1> Ask A Bored Guy <h1>
-     </td>
-    </tr>
+       <tr>
+        <td class="header" >
+           <h1> Ask A Bored Guy <h1>
+        </td>
+       </tr>
 
-    <tr>
-     <td>
-      <br>
-      <br>
-      <br>
-      <h2 style="color: #888888;"> {{ $body }} </h2>
-      <br>
-      <br>
-      <br>
-      <div class="ask-button">
+       <tr>
+        <td>
+         <br>
+         <br>
+         <br>
+         <h2> {{ $body }} </h2>
+         <br>
+         <br>
+         <br>
+         <div class="ask-button">
 
-         <a href="{{ url('/ask') }}">Ask Again</a>
+            <a href="{{ url('/ask') }}">Ask Again</a>
 
-      </div>
+         </div>
 
-      <br>
-      <br>
-     </td>
-    </tr>
+         <br>
+         <br>
+        </td>
+       </tr>
 
-    <tr>
-     <td bgcolor="#80bfff" class="footer" >
-        <br>
-        <p> Copyright - Ask A Bored Dude </p>
-        <br>
-     </td>
-    </tr>
+       <tr>
+        <td class="footer" >
+           <br>
+           <p> Copyright - Ask A Bored Dude </p>
+           <br>
+        </td>
+       </tr>
+
 
    </table>
 
