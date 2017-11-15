77e3ba0c95d6d0b52343c0c3690fde0e86e12ded
mahussensr <spawnificent@gmail.com>
2017-11-14 22:13:45 -0600

    trying new grid system for homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -68,11 +68,11 @@
 
    <div class="col-sm-12">
 <!-- ************************************ USER IMAGE AND STATS/MODALS************************************************** -->
-      <div class="col-sm-4">
+      <div class="col-sm-4 divLeft">
          <div class="row">
             <img src="{{ Storage::disk('s3')->url('profile_images/' . Auth::user()->profile_image) }}" class="profImg" style="border-radius: 50%;" data-toggle="modal" data-target="#popupLogin">
-            <h3 style="color: #4981ce; padding: 7px; float: left;"> {{ Auth::user()->user_name }} </h3>
-            <i class="fa fa-trophy fa-2x" style="color: gold;" style="float: right;" aria-hidden="true"></i>
+            <h2 style="color: #4981ce; padding: 7px;"> {{ Auth::user()->user_name }} </h2>
+            <i class="fa fa-trophy fa-2x" style="color: gold;" aria-hidden="true"></i>
          </div>
 
 
@@ -147,7 +147,7 @@
 
 <!-- ************************************ QUESTIONS SECTION ****************************************************************************************** -->
 
-      <div class="col-sm-4">
+      <div class="col-sm-4 divCenter">
 
          @if($questions->isEmpty())
             <div class="answerDiv">
