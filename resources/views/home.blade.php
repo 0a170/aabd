56cdfc70fdfde8a3cc9325dc9ceb9469bfc61dad
mahussensr <spawnificent@gmail.com>
2017-11-16 18:14:17 -0600

    trying to fix middle div in home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -181,7 +181,7 @@
 <!-- *************************************************************************************** -->
 
 
-      <div class="col-sm-4 divRight" style="right: 100%">
+      <div class="col-sm-4 divRight" style="right: 0;">
 
          <a href="https://twitter.com/askaboredguy" class="twitter-follow-button" data-show-count="false" style="margin: 0 auto; display: block;">Follow @askaboredguy</a>
 
