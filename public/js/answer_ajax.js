61056743517bad0ed4717e4b3a38b3b33ace3be2
mahussensr <spawnificent@gmail.com>
2017-11-26 15:01:19 -0600

    returning home page to normal

--- public/js/answer_ajax.js
+++ public/js/answer_ajax.js
@@ -47,8 +47,8 @@ $(document).ready(function() {
 
 		if(ansVal == '') {
 
-			//$('#' + failureID).show().html("Answer Cannot be empty");
-			$('#' + failureID).show();
+			$('#' + failureID).show().html("Answer Cannot be empty");
+			//$('#' + failureID).show();
 			setTimeout(function() {
 				$('#' + failureID).fadeOut("slow");
 				}, 3000);
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -174,7 +174,7 @@
                <br>
             </div>
             <br>
-            <div id="answer_failure{{ $question->question_id }}" class="ajax_failure"><p> Answer Cannot Be Empty </p></div>
+            <div id="answer_failure{{ $question->question_id }}" class="ajax_failure"></div>
             <div id="answer_success{{ $question->question_id }}" class="ajax_success"></div>
             <div id="server_error{{ $question->question_id }}" class="ajax_failure"></div>
             <br>
