4a5feab385b1265034dd15b977d6e2ab7fbbbd76
mahussensr <spawnificent@gmail.com>
2017-11-26 14:42:10 -0600

    returning home page to normal

--- public/js/answer_ajax.js
+++ public/js/answer_ajax.js
@@ -36,6 +36,7 @@ $(document).ready(function() {
 
 		//var failureID = $('#' + fID + ' :nth-child(8)').attr('id');
 		var failureID = $('#' + dID + ' :nth-child(3)').attr('id');
+		var failureP = $('#' + failureID + ' :nth-child(1)').attr('id');
 		//var successID = $('#' + fID + ' :nth-child(9)').attr('id');
 		var successID = $('#' + dID + ' :nth-child(4)').attr('id');
 		//var servFailureID = $('#' + fID + ' :nth-child(10)').attr('id');
@@ -46,7 +47,9 @@ $(document).ready(function() {
 
 		if(ansVal == '') {
 
-			$('#' + failureID).show().html("Answer Cannot be empty");
+			//$('#' + failureID).show().html("Answer Cannot be empty");
+			$('#' + failureP).text("Answer Cannot Be Empty");
+			$('#' + failureID).show();
 			setTimeout(function() {
 				$('#' + failureID).fadeOut("slow");
 				}, 3000);
