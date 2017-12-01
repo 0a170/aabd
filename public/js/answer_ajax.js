afa0bd0e641fb38c509028964296cf17a30460ee
mahussensr <spawnificent@gmail.com>
2017-12-01 04:02:06 -0600

    trying to fix answer validation message functionality

--- public/js/answer_ajax.js
+++ public/js/answer_ajax.js
@@ -38,12 +38,12 @@ $(document).ready(function() {
 
 		//var failureID = $('#' + fID + ' :nth-child(8)').attr('id');
 		//var failureID = $('#' + dID + ' :nth-child(3)').attr('id');
-		var statusID = $('#' + dID).next().next().attr('id');
+		var statusID = $('#' + dID).nextAll().eq(1).attr('id');
 		//var successID = $('#' + fID + ' :nth-child(9)').attr('id');
 		//var successID = $('#' + dID + ' :nth-child(4)').attr('id');
 		//var servFailureID = $('#' + fID + ' :nth-child(10)').attr('id');
       //var servFailureID = $('#' + dID + ' :nth-child(5)').attr('id');
-
+ 
 		//var token = $('#' + fID + ' :nth-child(4)').attr('id');
 		var token = $('#' + fID + ' :hidden:eq(2)').attr('id');
 
