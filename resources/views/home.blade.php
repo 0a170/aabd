7570c29f795d2d81b54314ae1f8797d5b6472257
mahussensr <spawnificent@gmail.com>
2017-12-02 16:48:20 -0600

    morning commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -169,7 +169,7 @@
                   <input type="hidden" value="{{ csrf_token() }}">
                   <br>
                   <input type="submit" class="btn btn-primary" id="ent{{ $question->question_id }}" name="theAnswer" value="Answer This">
-                  <div id="aStatus{{ $question->question_id }}" class="ans_status"></div>
+                  <span id="aStatus{{ $question->question_id }}" class="ans_status">&nbsp</span>
                </form>
                <br>
             </div>
