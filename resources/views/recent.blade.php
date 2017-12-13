c82585c32223c8b30f85f60f7aaa7a334c6de400
mahussensr <spawnificent@gmail.com>
2017-12-12 22:32:07 -0600

    restoring old env

--- resources/views/recent.blade.php
+++ resources/views/recent.blade.php
@@ -9,14 +9,13 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
 
 
 
 <script src="{{ asset('js/jquery-3.2.1.js') }}"></script>
 <script src="{{ asset('js/bootstrap.min.js') }}"></script>
-<script src="{{ secure_asset('js/rate_ajax.js') }}"></script>
+<script src="{{ asset('js/rate_ajax.js') }}"></script>
 
 
 </head>
