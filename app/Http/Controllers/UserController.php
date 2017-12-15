12585440e5e79d0965e379d20f15d293bb3de274
mahussensr <spawnificent@gmail.com>
2017-12-14 19:38:48 -0600

    trying to fix user icon images

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -68,6 +68,7 @@ class UserController extends Controller {
       //$data = [];
 
       $output = "";
+      $s3String = "";
 
       //if($req->has('q')){
           //$search = $req->q;
@@ -79,7 +80,8 @@ class UserController extends Controller {
 
           if($users) {
              foreach($users as $user)
-                $output .= '<tr class="user-table" style="border: 1px solid black; background: white; color: blue;"><td><a href="user/' . $user->id . '"><img src="{{ Storage::disk(\'s3\')->url(\'icons/icon_' . $user->profile_image . '\') }}" style="float: left;">' . $user->user_name . '</a></td></tr>';
+                $s3String = Storage::disk('s3')->url('icons/icon_' . $user->profile_image);
+                $output .= '<tr class="user-table" style="border: 1px solid black; background: white; color: blue;"><td><a href="user/' . $user->id . '"><img src="{{' . $s3String . '}}" style="float: left;">' . $user->user_name . '</a></td></tr>';
              }
       //}
 
